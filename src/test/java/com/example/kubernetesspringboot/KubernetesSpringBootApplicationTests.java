bdaf1abeab7ec28715196bcf172b9b2984e317d1
subhajit chatterjee <Subhajit.Chatterjee88@gmail.com>
2021-08-27 19:33:40 +0530

    initial commit

---
+++ src/test/java/com/example/kubernetesspringboot/KubernetesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kubernetesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
